4aaae3083a4ef6a112f6bbd57aba09374bb0884c
Xiaorui Dong <xiaorui@mit.edu>
2023-07-04 13:40:01 -0400

    Add basic properties related to the reaction template

--- rxntp/base.py
+++ rxntp/base.py
@@ -23,3 +23,51 @@ class ReactionTemplate(object):
         self.num_reactants = num_reactants
         self.num_products = num_products
 
+    @property
+    def num_atoms(self):
+        return self.graph.number_of_nodes()
+
+    @property
+    def num_bonds(self):
+        return self.graph.number_of_edges()
+
+    @property
+    def formed_bonds(self):
+        return [tuple(sorted((u, v))) for u, v, d in self.graph.edges(data='bond_change_type') if d == 'FORM_BOND']
+
+    @property
+    def broken_bonds(self):
+        return [tuple(sorted((u, v))) for u, v, d in self.graph.edges(data='bond_change_type') if d == 'BREAK_BOND']
+
+    @property
+    def non_react_bonds(self):
+        return [tuple(sorted((u, v))) for u, v, d in self.graph.edges(data='bond_order_change') if d == 0]
+
+    @property
+    def changed_bonds(self):
+        return [tuple(sorted((u, v))) for u, v, d in self.graph.edges(data='bond_change_type') if d == 'CHANGE_BOND']
+
+    @property
+    def is_charge_balance(self):
+        return sum([d['charge_change'] for _, d in self.graph.nodes(data=True)]) == 0
+
+    @property
+    def is_mult_equal(self):
+        return sum([d['radical_change'] for _, d in self.graph.nodes(data=True)]) == 0
+
+    @property
+    def num_formed_bonds(self):
+        return len(self.formed_bonds)
+
+    @property
+    def num_broken_bonds(self):
+        return len(self.broken_bonds)
+
+    @property
+    def num_non_react_bonds(self):
+        return len(self.non_react_bonds)
+
+    @property
+    def num_changed_bonds(self):
+        return len(self.changed_bonds)
+
